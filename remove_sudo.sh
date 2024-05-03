f8f0f4826d8d5ea8dc3fa1ce3d259ccc93410098
crashfistfight <crashfistfight@riseup.net>
2024-05-04 01:44:09 +0200

    update groups

--- remove_sudo.sh
+++ remove_sudo.sh
@@ -31,7 +31,7 @@ fi
 echo "root:$root_password" | chpasswd
 
 # remove user from group
-#usermod -rG wheel "$user"
+usermod -rG wheel "$user"
 
 # add user to group
 usermod -aG systemd-journal,libvirt,plugdev "$user"

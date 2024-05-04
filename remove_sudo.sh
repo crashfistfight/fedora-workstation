2ec010ca177469471f271477f255dce30a4fb643
crashfistfight <crashfistfight@riseup.net>
2024-05-05 00:05:37 +0200

    create groups, update groups, cleanup

--- remove_sudo.sh
+++ remove_sudo.sh
@@ -26,16 +26,33 @@ if [[ -z "$root_password" ]]; then
 fi
 
 
-### remove sudo ###
+### config user ###
 # set root password
 echo "root:$root_password" | chpasswd
 
 # remove user from group
 usermod -rG wheel "$user"
 
-# add user to group
-usermod -aG systemd-journal,libvirt "$user"
+# create groups
+groupadd plugdev
 
+# add user to group
+usermod -aG systemd-journal,libvirt,plugdev "$user"
+
+# create polkit rule for group diskadmin
+#tee /etc/polkit-1/rules.d/80-udisks2.rules > /dev/null <<EOF
+#polkit.addRule(function(action, subject) {
+#    if (action.id == "org.freedesktop.udisks2.encrypted-unlock-system" || action.id == "org.freedesktop.udisks2.filesystem-mount-system" || action.id == "org.freedesktop.udisks2.filesystem-unmount-system" &&
+#        subject.active == true && subject.local == true &&
+#        subject.isInGroup("diskadmin"))
+#        {
+#        return polkit.Result.YES;
+#    }
+#});
+#EOF
+
+
+### remove sudo ###
 # unlock package
 mv /etc/dnf/protected.d/{sudo.conf,sudo.conf.unlock}
 

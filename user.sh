3b1ce781afedb091fc775c2ca56ddc43a58a3861
crashfistfight <40041116+crashfistfight@users.noreply.github.com>
2024-06-10 01:17:55 +0200

    Restructure into subfolders (#4)
    * restructure into subfolders
    
    * fix snapper
    
    * fix snapper
    
    ---------
    
    Co-authored-by: crashfistfight <crashfistfight@riseup.net>

--- user.sh
+++ user.sh
@@ -1,18 +1,10 @@
 #! /usr/bin/env bash
 
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC2154
 
 ### config options ###
 # vars
-flatpaks=(
-#  'com.spotify.Client'
-  'com.yubico.yubioath'
-  'org.gimp.GIMP'
-  'org.signal.Signal'
-  'org.chromium.Chromium'
-  'com.bitwarden.desktop'
-  'com.visualstudio.code-oss'
-)
+source vars/all
 
 
 ### tests ###
@@ -28,59 +20,17 @@ fi
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # install user flatpaks
-flatpak --user install flathub --assumeyes --noninteractive --or-update "${flatpaks[@]}"
+flatpak --user install flathub --assumeyes --noninteractive --or-update "${user_flatpaks[@]}"
 
 # enable user flatpak auto-updates
 mkdir --parents "$HOME"/.config/systemd/user
 
-tee "$HOME"/.config/systemd/user/flatpak-automatic.service > /dev/null <<EOF
-[Unit]
-Description=Service for flatpak-automatic
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Type=oneshot
-Restart=on-failure
-RestartSec=60s
-
-# uninstall unused flatpaks
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --user uninstall --unused --assumeyes --noninteractive --delete-data
-
-# update
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --user update --assumeyes --noninteractive
-
-# repair
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --user repair
-EOF
-
-tee "$HOME"/.config/systemd/user/flatpak-automatic.timer > /dev/null <<EOF
-[Unit]
-Description=Trigger for flatpak-automatic.service
-
-[Timer]
-OnBootSec=1h
-OnUnitInactiveSec=1d
-OnCalendar=daily
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-EOF
+source files/user/flatpak
 
 systemctl --user daemon-reload
 systemctl --user enable --now flatpak-automatic.timer
 
 
 ### gnome ###
-# hide htop.desktop
-tee "$HOME"/.local/share/applications/htop.desktop > /dev/null <<EOF
-NoDisplay=true
-EOF
-
-# hide syncthing-start.desktop
-tee "$HOME"/.local/share/applications/syncthing-start.desktop > /dev/null <<EOF
-NoDisplay=true
-EOF
-
-
+# hide app icons
+source files/user/hide-app-icons

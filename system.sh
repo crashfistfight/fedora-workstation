72d0ed6c57771510d719226c338e92cb47a7b2df
crashfistfight <crashfistfight@riseup.net>
2024-06-09 23:38:47 +0200

    restructure into subfolders

--- system.sh
+++ system.sh
@@ -1,70 +1,10 @@
 #! /usr/bin/env bash
 
-# shellcheck disable=SC2154
+# shellcheck disable=SC2154,SC1091
 
 ### config options ###
 # vars
-packages=(
-  'htop'
-  'snapper'
-  'vim'
-  'syncthing'
-#  'dnf-automatic'
-  'nautilus'
-#  'gnome-console'
-  'virt-manager'
-  'borgmatic'
-  'android-tools'
-  'kernel-tools'
-  )
-
-removals=(
-  'gnome-boxes'
-  'gnome-clocks'
-  'gnome-characters'
-  'gnome-contacts'
-  'gnome-connections'
-  'gnome-font-viewer'
-  'gnome-software'
-  'gnome-logs'
-  'gnome-calendar'
-  'gnome-maps'
-  'gnome-tour'
-  'yelp'
-  'gnome-weather'
-  'gnome-text-editor'
-  'totem*'
-  'libreoffice*'
-  'rhythmbox'
-  'cheese'
-  'simple-scan'
-  'evince*'
-  'loupe'
-  'gnome-calculator'
-  'baobab'
-  'mediawriter'
-  'firefox'
-  'firefox-langpacks'
-#  'gnome-terminal'
-)
-
-flatpaks=(
-  'org.gnome.baobab'
-  'org.gnome.TextEditor'
-  'org.gnome.Evince'
-  'org.gnome.Calendar'
-  'org.gnome.Contacts'
-  'org.gnome.Logs'
-  'org.gnome.Loupe'
-  'org.gnome.Snapshot'
-  'org.gnome.Calculator'
-  'org.mozilla.firefox'
-  'org.freedesktop.Platform.ffmpeg-full/x86_64/23.08'
-  'org.gnome.Firmware'
-  'com.github.tchx84.Flatseal'
-#  'org.gnome.Geary'
-  'org.gnome.World.PikaBackup'
-)
+source vars/all
 
 
 ### tests ###
@@ -93,50 +33,10 @@ dnf install --assumeyes "${packages[@]}"
 #systemctl daemon-reload
 #systemctl enable --now dnf-automatic.timer
 
-tee /etc/systemd/system/dnf-offline-automatic.service > /dev/null <<EOF
-[Unit]
-Description=Start automatic offline updates on reboot
-Wants=network-online.target
-After=network-online.target
-StartLimitIntervalSec=10m
-StartLimitBurst=5
-
-[Service]
-Type=oneshot
-Restart=on-failure
-RestartSec=60s
-
-# refresh repo
-ExecStartPre=/usr/bin/pkcon refresh force
-
-# download updates
-ExecStart=-/usr/bin/pkcon update --only-download
-
-# prepare update
-ExecStart=-/usr/bin/pkcon offline-get-prepared
-
-# set magiclink
-ExecStartPost=-/usr/bin/pkcon offline-trigger
-EOF
-
-tee /etc/systemd/system/dnf-offline-automatic.timer > /dev/null <<EOF
-[Unit]
-Description=Timer for automatic offline updates on reboot
-Wants=network-online.target
-After=network-online.target
-
-[Timer]
-OnBootSec=1h
-OnUnitInactiveSec=1d
-OnCalendar=daily
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-EOF
+source files/system/dnf
 
 systemctl daemon-reload
-systemctl enable dnf-offline-automatic.timer
+systemctl enable --now dnf-offline-automatic.timer
 
 
 ### flatpaks ###
@@ -144,45 +44,10 @@ systemctl enable dnf-offline-automatic.timer
 flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # install system flatpaks
-flatpak --system install flathub --assumeyes --noninteractive --or-update "${flatpaks[@]}"
+flatpak --system install flathub --assumeyes --noninteractive --or-update "${system_flatpaks[@]}"
 
 # enable system flatpak auto-updates
-tee /etc/systemd/system/flatpak-automatic.service > /dev/null <<EOF
-[Unit]
-Description=Service for flatpak-automatic
-Wants=network-online.target
-After=network-online.target
-StartLimitIntervalSec=10m
-StartLimitBurst=5
-
-[Service]
-Type=oneshot
-Restart=on-failure
-RestartSec=60s
-
-# uninstall unused flatpaks
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --system uninstall --unused --assumeyes --noninteractive --delete-data
-
-# update
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --system update --assumeyes --noninteractive
-
-# repair
-ExecStart=/usr/bin/dbus-run-session /usr/bin/flatpak --system repair
-EOF
-
-tee /etc/systemd/system/flatpak-automatic.timer > /dev/null <<EOF
-[Unit]
-Description=Trigger for flatpak-automatic.service
-
-[Timer]
-OnBootSec=1h
-OnUnitInactiveSec=1d
-OnCalendar=daily
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-EOF
+source files/system/flatpak
 
 systemctl daemon-reload
 systemctl enable --now flatpak-automatic.timer
@@ -190,11 +55,7 @@ systemctl enable --now flatpak-automatic.timer
 
 ### bash ###
 # configure bash prompt
-tee /etc/profile.d/prompt.sh > /dev/null <<EOF
-#! /usr/bin/env bash
-
-export PS1="[\[\e[31m\]\u\[\e[m\]@\h] \[\e[01;31m\]:\[\e[m\]\[\e[01;31m\]:\[\e[m\] \W \[\e[01;31m\]>\[\e[m\] "
-EOF
+source files/system/bash
 
 
 ### snapper ###
@@ -202,43 +63,7 @@ EOF
 mkdir --parents /etc/systemd/system/snapper-{cleanup.timer.d,timeline.timer.d}
 mkdir --parents /etc/snapper/configs
 
-  tee /etc/systemd/system/snapper-cleanup.timer.d/override.conf > /dev/null <<EOF
-[Timer]
-OnBootSec=
-OnUnitActiveSec=
-OnCalendar=hourly
-Persistent=true
-EOF
-
-tee /etc/systemd/system/snapper-timeline.timer.d/override.conf > /dev/null <<EOF
-[Timer]
-Persistent=true
-EOF
-
-tee /etc/snapper/configs/home > /dev/null <<EOF
-# subvolume to snapshot
-SUBVOLUME="$(lsblk --output MOUNTPOINTS | grep 'home$')"
-
-# filesystem type
-FSTYPE="btrfs"
-
-# create hourly snapshots
-TIMELINE_CREATE="yes"
-
-# cleanup hourly snapshots after some time
-TIMELINE_CLEANUP="yes"
-
-# limits for timeline cleanup
-TIMELINE_MIN_AGE="1800"
-TIMELINE_LIMIT_HOURLY="24"
-TIMELINE_LIMIT_DAILY="3"
-TIMELINE_LIMIT_WEEKLY="0"
-TIMELINE_LIMIT_MONTHLY="0"
-TIMELINE_LIMIT_YEARLY="0"
-
-# cleanup empty pre-post-pairs
-EMPTY_PRE_POST_CLEANUP="yes"
-EOF
+source files/system/snapper
 
 sed --in-place "s@^SNAPPER_CONFIGS=.*@SNAPPER_CONFIGS=\"home\"@" /etc/sysconfig/snapper
 
@@ -248,30 +73,4 @@ systemctl enable --now snapper-timeline.timer snapper-cleanup.service
 
 ### vim ###
 # configure vim
-tee /etc/vimrc > /dev/null <<EOF
-set autoindent
-set autoread
-set autowrite
-set expandtab
-set hidden
-set ignorecase
-set incsearch
-set laststatus=2
-set linebreak
-set list listchars=tab:▸\ ,trail:·
-set nofoldenable
-set nojoinspaces
-set number
-set path+=**
-set printoptions=paper:A4,syntax:n,number:y
-set shiftwidth=4
-set showbreak=↪\
-set splitbelow
-set splitright
-set statusline=\(%n\)\ %<%.99f\ %y\ %w%m%r%=%-14.(%l,%c%V%)\ %P
-set tabstop=4
-set textwidth=120
-set wrapscan
-EOF
-
-
+source files/system/vim
